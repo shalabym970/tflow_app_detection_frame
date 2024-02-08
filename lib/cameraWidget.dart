1f1e3eda1379721247169e2d2d3f8b172e1d6bcf
shalaby <shalabym970@gmail.com>
2024-02-08 13:25:37 +0200

    clear all models and set just one

---
+++ lib/cameraWidget.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:math' as math;
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'models.dart';
+
+typedef void Callback(List<dynamic> list, int h, int w);
+
+class CameraWidget extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  final Callback setRecognitions;
+  final String model;
+
+  CameraWidget(this.cameras, this.model, this.setRecognitions);
+
+  @override
+  _CameraWidgetState createState() => new _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  CameraController? controller;
+  bool isDetecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.cameras.length < 1) {
+      print('No camera is found');
+    } else {
+      controller = new CameraController(
+        widget.cameras[0],
+        ResolutionPreset.high,
+      );
+      controller?.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+
+        controller?.startImageStream((CameraImage img) {
+          if (!isDetecting) {
+            isDetecting = true;
+
+            int startTime = new DateTime.now().millisecondsSinceEpoch;
+
+            Tflite.detectObjectOnFrame(
+              bytesList: img.planes.map((plane) {
+                return plane.bytes;
+              }).toList(),
+              model: "SSDMobileNet",
+              imageHeight: img.height,
+              imageWidth: img.width,
+              imageMean: 127.5,
+              imageStd: 127.5,
+              numResultsPerClass: 1,
+              threshold: widget.model == yolo ? 0.2 : 0.4,
+            ).then((recognitions) {
+              int endTime = new DateTime.now().millisecondsSinceEpoch;
+              print("Detection took ${endTime - startTime}");
+              print(
+                  '========== recognitions : ${recognitions.toString()} ========== ');
+              widget.setRecognitions(recognitions!, img.height, img.width);
+              isDetecting = false;
+            });
+          }
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await controller?.stopImageStream();
+    await Tflite.close();
+    controller?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
+
+    Size? tmp = MediaQuery.of(context).size;
+    var screenH = math.max(tmp.height, tmp.width);
+    var screenW = math.min(tmp.height, tmp.width);
+    tmp = controller!.value.previewSize;
+    var previewH = math.max(tmp!.height, tmp.width);
+    var previewW = math.min(tmp.height, tmp.width);
+    var screenRatio = screenH / screenW;
+    var previewRatio = previewH / previewW;
+
+    return OverflowBox(
+      maxHeight:
+          screenRatio > previewRatio ? screenH : screenW / previewW * previewH,
+      maxWidth:
+          screenRatio > previewRatio ? screenH / previewH * previewW : screenW,
+      child: CameraPreview(controller!),
+    );
+  }
+}
